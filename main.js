921fad5fd75fd681b4bdfb179efe49c337efb8d6
diegoqff <41656532+diegoqff@users.noreply.github.com>
2020-12-02 23:35:01 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+$('.projetos').slick({
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  infinite: true
+});
+
+$('.galeria').slick({
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  dots: true,
+  infinite: true
+});
